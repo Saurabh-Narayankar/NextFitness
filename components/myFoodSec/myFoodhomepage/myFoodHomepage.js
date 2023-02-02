f0006e7d81c5951ba36f62ff2ed462bb6ddcebb3
DESKTOP-33BVUNN <narayankarsaurabh1@gmail.com>
2023-02-02 05:30:12 +0530

    added some changes

--- components/myFoodSec/myFoodhomepage/myFoodHomepage.js
+++ components/myFoodSec/myFoodhomepage/myFoodHomepage.js
@@ -65,10 +65,10 @@ const MyFoodHomepage = () => {
             <div className={classes.foodHomepageContainer}>
                 <div className={classes.foodHomepageInnerContainer}>
                     <div className={classes.TotalTab}>
-                        <p><span className={classes.TotalTabTitle}>Total Calories:--</span> {foodData.totalCalories.toFixed(2)}</p>
-                        <p><span className={classes.TotalTabTitle}>Total Carbs:--</span> {foodData.totalCarb.toFixed(2)}</p>
-                        <p><span className={classes.TotalTabTitle}>Total Protein:--</span> {foodData.totalProtein.toFixed(2)}</p>
-                        <p><span className={classes.TotalTabTitle}>Total Fat:--</span> {foodData.totalFat.toFixed(2)}</p>
+                        <p><span className={classes.TotalTabTitle}>Total Calories:--</span> {Math.round(foodData.totalCalories)}</p>
+                        <p><span className={classes.TotalTabTitle}>Total Carbs:--</span> {Math.round(foodData.totalCarb)}</p>
+                        <p><span className={classes.TotalTabTitle}>Total Protein:--</span> {Math.round(foodData.totalProtein)}</p>
+                        <p><span className={classes.TotalTabTitle}>Total Fat:--</span> {Math.round(foodData.totalFat)}</p>
                     </div>
                     <div className={classes.meals}>
                         <p className={classes.mealTitle}>Breakfast</p>
@@ -133,41 +133,3 @@ const MyFoodHomepage = () => {
 }
 
 export default MyFoodHomepage;
-
-
-{/* <div>
-                <h3>lunch</h3>
-                <div className={classes.breakfast}>
-                    {foodData.lunch && foodData.lunch.length !== 0 ? 
-                    (foodData.lunch.map((foodItem, index) => 
-                        <p key={index}>{foodItem.name}, {foodItem.servingSize}, {foodItem.carbs}</p>
-                    )) 
-                    : 
-                    (<p>add food items for lunch</p>)}
-                    <Link href={`/myFood/addFood/lunch/${day}`}><button>add food</button></Link>
-                </div>
-            </div>
-            <div>
-                <h3>dinner</h3>
-                <div className={classes.breakfast}>
-                    {foodData.dinner && foodData.dinner.length !== 0 ?
-                    (foodData.dinner.map((foodItem, index) => 
-                        <p key={index}>{foodItem.name}, {foodItem.servingSize}, {foodItem.carbs}</p>
-                    )) 
-                    : 
-                    (<p>add food items for dinner</p>)}
-                    <Link href={`/myFood/addFood/dinner/${day}`}><button>add food</button></Link>
-                </div>
-            </div>
-            <div>
-                <h3>snacks</h3>
-                <div className={classes.breakfast}>
-                    {foodData.snacks && foodData.snacks.length !== 0 ?
-                    (foodData.snacks.map((foodItem, index) => 
-                        <p key={index}>{foodItem.name}, {foodItem.servingSize}, {foodItem.carbs}</p>
-                    )) 
-                    : 
-                    (<p>add food items for snacks</p>)}
-                    <Link href={`/myFood/addFood/snacks/${day}`}><button>add food</button></Link>
-                </div>
-            </div> */}
