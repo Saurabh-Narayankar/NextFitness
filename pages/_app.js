d9f420ff2d21ef5961d1345c0ca5ac813945f169
DESKTOP-33BVUNN <narayankarsaurabh1@gmail.com>
2023-02-07 16:56:53 +0530

    added title for web app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
+import Head from "next/head";
 import Navbar from '../components/navbar/navbar'
 
 import { UserProvider } from '../context/userContext'
+
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return(
     <>
+      <Head>
+        <title>NextFitness</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <UserProvider>
         <Navbar />
         <Component {...pageProps} />
